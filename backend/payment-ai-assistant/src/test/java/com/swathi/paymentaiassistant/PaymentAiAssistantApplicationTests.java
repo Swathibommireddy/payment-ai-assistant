c7455ff3050fbe8e8c08d0fdb4fd91768464f0aa
Swathibommireddy <swathibommireddy@gmail.com>
2026-07-13 17:39:58 +0800

    Intial Spring boot project setup

---
+++ backend/payment-ai-assistant/src/test/java/com/swathi/paymentaiassistant/PaymentAiAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swathi.paymentaiassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentAiAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
